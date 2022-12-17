5d85884e68217c797ceb91d24fdf971bad3e3b2d
therealmn9 <mannynyong@outlook.com>
2022-12-17 02:45:49 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return - Always 0
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return - 0 if exited properly, non zero otherwise
  */
 int main(void)
 {
